c2aafa99c9392f34fc96e735516f977fa03e4c52
冷魅 <56126841+Anonym10086@users.noreply.github.com>
2019-11-17 23:41:53 +0800

    Create index.php

---
+++ pc/index.php
@@ -0,0 +1,12 @@
+<?php
+echo '
+<html>
+<head>
+<title>冷魅win7</title>
+</head>
+<body style="margin: 0;">
+<iframe src="http://xn--b8q260p.com/win7/win7.html" frameborder="0" width="100%" scrolling="off" height="100%" leftmargin="0" topmargin="0">
+</iframe>
+</body>
+</html>';
+?>
